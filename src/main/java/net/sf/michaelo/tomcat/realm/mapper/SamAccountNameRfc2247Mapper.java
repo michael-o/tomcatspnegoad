f36f148f0ad1b2af7c6015290340d3160a7c2233
Michael Osipov <1983-01-06@gmx.net>
2015-05-13 12:33:41 +0000

    Documented use/mimic of DsCrackNames

--- src/main/java/net/sf/michaelo/tomcat/realm/mapper/SamAccountNameRfc2247Mapper.java
+++ src/main/java/net/sf/michaelo/tomcat/realm/mapper/SamAccountNameRfc2247Mapper.java
@@ -33,10 +33,16 @@ import org.ietf.jgss.GSSName;
  * GSS name in the primary and realm components. The instance component is completely
  * ignored. The primary component is assigned to the {@code sAMAccountName} and the realm is
  * transformed to a search base according to <a href="http://www.ietf.org/rfc/rfc2247.txt">RFC
- * 2247</a>. <br/>
+ * 2247</a>.
+ * <p>
  * This mapper requires to operate from the {@code RootDSE} of a domain controller or better yet, a
  * GC. No root DN normalization (stripping DC components) happens here (yet).
  *
+ * Moreover, this implementation mimics <a href="https://msdn.microsoft.com/en-us/library/ms675970%28v=vs.85%29.aspx">{@code DsCrackNames}</a>
+ * with {@code formatOffered} set to {@code DS_USER_PRINCIPAL_NAME} and {@code formatDesired} set to
+ * {@code DS_FQDN_1779_NAME}. Verified against <a href="https://github.com/jelmer/samba/blob/7ed24924d2917556a03c51eadcb65b3e3c1e8af6/source4/dsdb/samdb/cracknames.c#L1260">Samba's implementation</a>
+ * of {@code DsCrackNames}.
+ *</p>
  * @version $Id$
  */
 public class SamAccountNameRfc2247Mapper extends SamAccountNameMapper {
