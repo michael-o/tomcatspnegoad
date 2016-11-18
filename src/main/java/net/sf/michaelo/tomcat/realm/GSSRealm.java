f855fef31a5b956774db0a2177412559b6ea1c77
Michael Osipov <1983-01-06@gmx.net>
2016-11-18 10:18:41 +0000

    Add a GSSRealm interface with the most important methods

---
+++ src/main/java/net/sf/michaelo/tomcat/realm/GSSRealm.java
@@ -0,0 +1,47 @@
+package net.sf.michaelo.tomcat.realm;
+
+import java.security.Principal;
+
+import org.ietf.jgss.GSSContext;
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSName;
+
+/**
+ * Realm definition which is able to retrieve principals from {@link GSSName GSS names} or fully
+ * established {@link GSSContext GSS contexts}.
+ *
+ * @version $Id$
+ */
+public interface GSSRealm {
+
+	/**
+	 * Authenticates a user from a given GSS name.
+	 *
+	 * @param gssName
+	 *            the GSS name of the context initiator (client)
+	 * @param gssCredential
+	 *            the GSS credential of the context initiator (client)
+	 * @return the retrieved principal
+	 * @throws NullPointerException
+	 *             if the gssName is null
+	 */
+	// TODO Create issue for this to be added into Tomcat's Realm
+	Principal authenticate(GSSName gssName, GSSCredential gssCredential);
+
+	/**
+	 * Authenticates a user from a fully established GSS context.
+	 *
+	 * @param gssContext
+	 *            the GSS context established with the peer
+	 * @param storeCreds
+	 *            the store delegated credential indication
+	 * @return the retrieved principal
+	 * @throws NullPointerException
+	 *             if the gssContext is null
+	 * @throws IllegalStateException
+	 *             if the gssContext is not fully established
+	 */
+	// TODO Remove this method in the next iteration. It is already in RealmBase
+	Principal authenticate(GSSContext gssContext, boolean storeCreds);
+
+}
