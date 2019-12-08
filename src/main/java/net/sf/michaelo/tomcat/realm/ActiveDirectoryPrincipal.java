bdc893cc730dd9ab505a7e9aab9a9a3ee91920fd
Michael Osipov <1983-01-06@gmx.net>
2019-12-08 13:09:04 +0000

    Align ActiveDirectoryRealm#hasRole(String) with GenericPrincipal

--- src/main/java/net/sf/michaelo/tomcat/realm/ActiveDirectoryPrincipal.java
+++ src/main/java/net/sf/michaelo/tomcat/realm/ActiveDirectoryPrincipal.java
@@ -118,6 +118,10 @@ public class ActiveDirectoryPrincipal implements TomcatPrincipal {
 	 * @return true if principal is associated with the role, else false
 	 */
 	public boolean hasRole(String role) {
+		if ("*".equals(role)) // Special 2.4 role meaning everyone
+			return true;
+		if (role == null)
+			return false;
 		return Arrays.binarySearch(roles, role) >= 0;
 	}
 
